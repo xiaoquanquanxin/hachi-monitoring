7799923fa1bf0b45eac7ba4302bb8eda8ff604ef
xiaoquanquanxin <xxkl123@126.com>
2020-12-11 18:28:07 +0800

    初始化

---
+++ src/layout/mainContent.vue
@@ -0,0 +1,19 @@
+<template>
+    <a-layout>
+        <a-layout-content class="layout-content">
+            <router-view></router-view>
+        </a-layout-content>
+    </a-layout>
+</template>
+<script>
+    export default {
+        name: 'MainContent'
+    };
+</script>
+<style scoped lang="less">
+    .layout-content {
+        margin: 0;
+        padding: 24px;
+        background: #fff;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,19 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { syncRouteList } from './syncRouteList';
+import getPageTitle from '@/utils/utils';
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: syncRouteList
+});
+router.beforeEach(async (to, from, next) => {
+    //	console.log('来自哪儿', from.path);
+    //	console.log('去哪儿', to.path);
+    window.document.title = getPageTitle(to.meta);
+    next();
+});
 
-export default router
+export default router;
