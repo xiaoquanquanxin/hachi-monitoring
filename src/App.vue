58b8ed2cd9c354138ab0a11ad1066fc5cf17a354
xiaoquanquanxin <xxkl123@126.com>
2020-12-11 17:23:26 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <a-layout id="components-layout-demo-custom-trigger">
+        <Slider/>
+        <MainContent/>
+    </a-layout>
 </template>
+<script>
+    import Slider from '@/layout/slider.vue';
+    import MainContent from '@/layout/mainContent.vue';
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    export default {
+        components: {
+            Slider,
+            MainContent,
+        },
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    };
+</script>
+<style scoped lang="less">
+    #components-layout-demo-custom-trigger {
+        .trigger {
+            font-size: 18px;
+            line-height: 64px;
+            padding: 0 24px;
+            cursor: pointer;
+            transition: color 0.3s;
+            
+            &:hover {
+                color: #2d3237;
+            }
+        }
     }
-  }
-}
 </style>
